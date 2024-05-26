59a4cf564313819403c018e2263d3279eeeadc0b
Gaurab1010 <gaurabneupane8969@gmail.com>
2024-05-26 13:45:45 +0545

    images changes

--- core/settings.py
+++ core/settings.py
@@ -127,7 +127,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIR = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'vege', 'static')
 ]
 
 MEDIA_URL = '/media/'
